b187edd3e5b9d54b77bb7e344ebd648c9eb3773f
Radhesk2000 <112677022+Radhesk2000@users.noreply.github.com>
2022-10-13 19:03:19 +0530

    Add files via upload

---
+++ ReverseStringMain.java
@@ -0,0 +1,28 @@
+
+/*
+ * 2.	Write a method reverse() that receives a string str as a parameter 
+ * and returns the characters in str in reverse order. Thus, 
+ * if the value sent to reverse() is "I me ", the method returns " em I".
+ */
+package com.string.test;
+import java.util.Scanner;
+import com.string.test.ReverseString;
+
+public class ReverseStringMain {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);	//creating object
+		
+		ReverseString rs = new ReverseString();
+		
+		System.out.println("Enter a String: ");
+		String s = sc.nextLine();
+		
+		
+		System.out.println("Reverse: " + rs.reverse(s)); //print reverse String.
+		
+		sc.close();
+	}
+
+}
